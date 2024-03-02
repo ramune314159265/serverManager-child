66430ecb78afd173c5db869367495cf75536303d
ramune314159265 <ramune314159265@gmail.com>
2024-03-03 00:05:14 +0900

    切り出し

---
+++ src/websocket/dataReceived.ts
@@ -0,0 +1,109 @@
+import WebSocket from 'websocket'
+
+import { servers } from '..'
+import { receivedData, sendData } from './interfaces'
+import { isIPty } from '../server/interfaces'
+import { allStop } from '../server/allstop'
+
+export const receivedHandle = (data: receivedData, connection: WebSocket.connection) => {
+	switch (data.type) {
+		case 'server_start': {
+			if (typeof data.serverId !== 'string') {
+				return
+			}
+			if (!Object.hasOwn(servers, data.serverId)) {
+				return
+			}
+			const server = servers[data.serverId]
+			server.start()
+
+			if (!isIPty(server.process)) {
+				return
+			}
+
+			server.process.onData(data => {
+				const dataToSend: sendData = {
+					type: 'server_stdout',
+					serverId: server.id,
+					content: data
+				}
+				connection.send(JSON.stringify(dataToSend))
+			})
+			server.process.onExit(() => {
+				const dataToSend: sendData = {
+					type: 'server_stopped',
+					serverId: server.id,
+				}
+				connection.send(JSON.stringify(dataToSend))
+			})
+
+			const dataToSend: sendData = {
+				type: 'server_started',
+				serverId: server.id
+			}
+			connection.send(JSON.stringify(dataToSend))
+		}
+			break
+		case 'server_write_stdin': {
+			if (typeof data.serverId !== 'string') {
+				return
+			}
+			if (!Object.hasOwn(servers, data.serverId)) {
+				return
+			}
+			const server = servers[data.serverId]
+			if (!isIPty(server.process)) {
+				return
+			}
+			if (typeof data.content !== 'string') {
+				return
+			}
+
+			server.process.write(data.content)
+		}
+			break
+		case 'server_stop': {
+			if (typeof data.serverId !== 'string') {
+				return
+			}
+			if (!Object.hasOwn(servers, data.serverId)) {
+				return
+			}
+			const server = servers[data.serverId]
+			if (server.process === null) {
+				return
+			}
+
+			server.stop()
+		}
+			break
+
+		case 'server_hard_stop': {
+			if (typeof data.serverId !== 'string') {
+				return
+			}
+			if (!Object.hasOwn(servers, data.serverId)) {
+				return
+			}
+			const server = servers[data.serverId]
+			if (server.process === null) {
+				return
+			}
+
+			server.hardStop()
+		}
+			break
+
+		case 'stop': {
+			allStop()
+				.then(() => {
+					connection.close()
+					process.exit(0)
+				})
+		}
+			break
+
+		default:
+			break
+	}
+}
--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,9 +1,7 @@
 import WebSocket from 'websocket'
 
-import { servers } from '..'
-import { receivedData, sendData } from './interfaces'
-import { isIPty } from '../server/interfaces'
-import { allStop } from '../server/allstop'
+import { receivedData } from './interfaces'
+import { receivedHandle } from './dataReceived'
 
 export const wsClient: WebSocket.client = new WebSocket.client
 
@@ -18,105 +16,7 @@ wsClient.on('connect', connection => {
 		}
 
 		const data: receivedData = JSON.parse(message.utf8Data)
-		switch (data.type) {
-			case 'server_start': {
-				if (typeof data.serverId !== 'string') {
-					return
-				}
-				if (!Object.hasOwn(servers, data.serverId)) {
-					return
-				}
-				const server = servers[data.serverId]
-				server.start()
 
-				if (!isIPty(server.process)) {
-					return
-				}
-
-				server.process.onData(data => {
-					const dataToSend: sendData = {
-						type: 'server_stdout',
-						serverId: server.id,
-						content: data
-					}
-					connection.send(JSON.stringify(dataToSend))
-				})
-				server.process.onExit(() => {
-					const dataToSend: sendData = {
-						type: 'server_stopped',
-						serverId: server.id,
-					}
-					connection.send(JSON.stringify(dataToSend))
-				})
-
-				const dataToSend: sendData = {
-					type: 'server_started',
-					serverId: server.id
-				}
-				connection.send(JSON.stringify(dataToSend))
-			}
-				break
-			case 'server_write_stdin': {
-				if (typeof data.serverId !== 'string') {
-					return
-				}
-				if (!Object.hasOwn(servers, data.serverId)) {
-					return
-				}
-				const server = servers[data.serverId]
-				if (!isIPty(server.process)) {
-					return
-				}
-				if (typeof data.content !== 'string') {
-					return
-				}
-
-				server.process.write(data.content)
-			}
-				break
-			case 'server_stop': {
-				if (typeof data.serverId !== 'string') {
-					return
-				}
-				if (!Object.hasOwn(servers, data.serverId)) {
-					return
-				}
-				const server = servers[data.serverId]
-				if (server.process === null) {
-					return
-				}
-
-				server.stop()
-			}
-				break
-
-			case 'server_hard_stop': {
-				if (typeof data.serverId !== 'string') {
-					return
-				}
-				if (!Object.hasOwn(servers, data.serverId)) {
-					return
-				}
-				const server = servers[data.serverId]
-				if (server.process === null) {
-					return
-				}
-
-				server.hardStop()
-			}
-				break
-
-			case 'stop': {
-				allStop()
-					.then(() => {
-						connection.close()
-						process.exit(0)
-					})
-			}
-				break
-
-			default:
-				break
-		}
+		receivedHandle(data, connection)
 	})
 })
