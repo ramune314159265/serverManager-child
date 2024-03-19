5b40ff4df31665b76ee721e2da4f705105caa96d
ramune314159265 <ramune314159265@gmail.com>
2024-03-19 13:20:30 +0900

    nat-api追加

---
+++ src/@types/@silentbot1/nat-api/index.d.ts
@@ -0,0 +1,29 @@
+declare module '@silentbot1/nat-api' {
+	interface INatAPIOptions {
+		ttl?: number,
+		autoUpdate?: boolean,
+		gateway: string,
+		enablePMP?: boolean,
+		enableUPNP?: boolean,
+		upnpPermanentFallback?: boolean
+	}
+	interface IPortOptions {
+		publicPort: number
+		privatePort: number
+		protocol: string | null
+		ttl?: number
+		description?: string
+	}
+
+	export default class NatAPI {
+		constructor(opts: INatAPIOptions)
+		map(port: number): Promise<void>
+		map(publicPort: number, privatePort: number): Promise<void>
+		map(opts: IPortOptions): Promise<void>
+		unmap(port: number): Promise<void>
+		unmap(publicPort: number, privatePort: number): Promise<void>
+		unmap(opts: IPortOptions): Promise<void>
+		externalIp(): Promise<string>
+		destroy(): Promise<void>
+	}
+}
